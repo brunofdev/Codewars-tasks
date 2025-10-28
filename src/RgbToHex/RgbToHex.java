03e43aba4bc10a79f329f37e5d246b6fde50fb11
Bruno de Fraga <brunofragaa97@gmail.com>
2025-10-27 23:29:31 -0300

    adicionado descricao do problema ao problema RgbToHex

--- src/RgbToHex/RgbToHex.java
+++ src/RgbToHex/RgbToHex.java
@@ -1,5 +1,23 @@
 package RgbToHex;
 
+
+        /**
+         * Aqui está a tradução:
+         *
+         * A função rgb está incompleta. Complete-a para que,
+         * ao passar valores decimais RGB, resulte em uma representação
+         * hexadecimal sendo retornada. Valores decimais válidos para RGB
+         * são de 0 a 255. Quaisquer valores que fiquem fora desse intervalo
+         * devem ser arredondados para o valor válido mais próximo.
+         *
+         * Observação: Sua resposta deve sempre ter 6 caracteres de comprimento,
+         * a forma abreviada com 3 não funcionará aqui.
+         *
+         * 255, 255, 255 --> "FFFFFF"
+         * 255, 255, 300 --> "FFFFFF"
+         * 0, 0, 0       --> "000000"
+         * 148, 0, 211   --> "9400D3"
+        **/
 //solução que eu fiz
 public class RgbToHex {
     public static String rgb(int r, int g, int b){
