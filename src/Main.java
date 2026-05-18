a1312b638846e4c05b5039254a483ee596c443a7
Bruno Fraga <brunofragaa97@gmail.com>
2026-05-18 14:12:44 -0300

    KATA: Algoritmo que resolve kata Vowel Count, contagem de vogais.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,17 @@
-import ConvertStringToCamelCase.ConvertStringToCamelCase;
-import CountingDuplicates.CountingDuplicates;
-import CreatePhoneNumber.CreatePhoneNumber;
-import DecondingOrder.DescendingOrder;
-import FindTheParityOutlier.FindTheParityOutlier;
+
 import PangramCheck.PangramCheck;
-import RgbToHex.RgbToHex;
-import SquareDigit.SquareDigit;
-import TakeAtenMinutesWalk.TakeTenMinutesWalk;
-import WhoLikesIt.Wholikeit;
+import vowel_count.VowelCount;
 
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static WhoLikesIt.Wholikeit.whoLikesIt;
 
 public class Main {
     public static void main(String[] args) {
 
-        IO.println("A frase passada é Pangram? " + PangramCheck.check2("The quick222213123 brown fo jumps over the lazy dog"));
+        int resultado = VowelCount.solucao3("TESTE dE VOGAIS");
+
+        System.out.println(resultado);
+
 
 
     }
+
 }
---
+++ target/generated-sources/annotations/vowel_count/jmh_generated/VowelCountBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package vowel_count.jmh_generated;
+public class VowelCountBenchmark_jmhType extends VowelCountBenchmark_jmhType_B3 {
+}
+
