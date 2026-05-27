bedd57fdb4bbed513f0ae431b83e12b18d0540b0
Bruno de Fraga <brunofragaa97@gmail.com>
2026-05-27 19:01:17 -0300

    Kata: Resolve kata Teknonymy

--- src/main/java/br/com/brunofragadev/Main.java
+++ src/main/java/br/com/brunofragadev/Main.java
@@ -1,35 +1,31 @@
 package br.com.brunofragadev;
+import br.com.brunofragadev.teknonymy.Person;
+import br.com.brunofragadev.teknonymy.Teknonymy;
+import java.time.LocalDateTime;
 
-
-import br.com.brunofragadev.campeonato_tree_train.CampeonatoTree;
-import br.com.brunofragadev.campeonato_tree_train.Node;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Node jogo1 = new Node("Flamengo", "Palmeiras", null, "Quartas de final");
-        Node jogo2 = new Node("Gremio", "Inter", null, "Quartas de final");
-        Node jogo3 = new Node("Atletico mineiro", "Conrinthians", null, "Quartas de final");
-        Node jogo4 = new Node("Juventude", "Caxias", null,"Quartas de final");
+        // Geração 3 - Netos
+        Person ana     = new Person("Ana",     'f', new Person[]{}, LocalDateTime.of(1043, 11, 1, 0, 0, 0));
+        Person pedro   = new Person("Pedro",   'm', new Person[]{}, LocalDateTime.of(1045, 1, 1, 0, 0, 0));
+        Person lucas   = new Person("Lucas",   'm', new Person[]{}, LocalDateTime.of(1046, 1, 1, 0, 0, 0));
+        Person maria   = new Person("Maria",   'f', new Person[]{}, LocalDateTime.of(1047, 1, 1, 0, 0, 0));
 
-        Node semi1 = new Node(null, null, null, "Semi final");
-        semi1.setEsquerda(jogo1);
-        semi1.setDireita(jogo2);
+        // Geração 2 - Filhos
+        Person beatriz = new Person("Beatriz", 'f', new Person[]{maria},          LocalDateTime.of(1020, 1, 1, 0, 0, 0));
+        Person carlos  = new Person("Carlos",  'm', new Person[]{lucas},          LocalDateTime.of(1021, 2, 1, 0, 0, 0));
+        Person daniel  = new Person("Daniel",  'm', new Person[]{pedro, ana},     LocalDateTime.of(1023, 11, 28, 0, 0, 0));
 
-        Node semi2 = new Node(null, null, null, "Semi final");
-        semi2.setEsquerda(jogo3);
-        semi2.setDireita(jogo4);
+        // Geração 1 - Avô
+        Person joao    = new Person("Joao",    'm', new Person[]{beatriz, carlos, daniel}, LocalDateTime.of(1000, 1, 1, 0, 0, 0));
 
-        Node final1 = new Node(null, null, null, "Grande final do campeonado: ");
-        final1.setEsquerda(semi1);
-        final1.setDireita(semi2);
+        Teknonymy t = new Teknonymy();
+        t.compute(joao);
 
-        CampeonatoTree campeonato = new CampeonatoTree(final1);
-        campeonato.simularCampeonato(final1);
-
-        System.out.println("O vencedor do campeonato é: " + campeonato.getRaiz().getResultado());
-
-        }
+        System.out.println(joao.teknonym);    // grandfather of Ana
+        System.out.println(beatriz.teknonym); // mother of Maria
+        System.out.println(daniel.teknonym);  // father of Ana
+        System.out.println(carlos.teknonym);  // father of Lucas
     }
+}
